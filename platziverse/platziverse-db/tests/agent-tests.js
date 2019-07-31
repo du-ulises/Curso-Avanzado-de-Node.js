3a9d9163b7d4f1e43037ec45648250fe2e5e3503
Diego Ulises <diegoulisesmartinezaguilar@gmail.com>
2019-07-31 18:49:19 -0500

    Implementación de createOrUpdate

--- platziverse/platziverse-db/tests/agent-tests.js
+++ platziverse/platziverse-db/tests/agent-tests.js
@@ -16,9 +16,16 @@ let MetricStub = {
 
 let single = Object.assign({}, agentFixtures.single)
 let id = 1
+let uuid = 'yyy-yyy-yyy'
 let AgentStub = null
 let db = null
 let sandbox = null
+
+let uuidArgs = {
+  where: {
+    uuid
+  }
+}
 
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
@@ -27,9 +34,17 @@ test.beforeEach(async () => {
     hasMany: sandbox.spy()
   }
 
+  //Model findOne Stub
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+
   //Model findById Stub
   AgentStub.findById = sandbox.stub()
   AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
+  //Model update Stub
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
 
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
@@ -62,4 +77,14 @@ test.serial('Agent#findById', async t => {
   t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
 
   t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
+})
+
+test.serial('Agent#createOrUpdate - exists', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+
+  t.true(AgentStub.findOne.called, 'findOne should be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne should be called twice')
+  t.true(AgentStub.update.calledOnce, 'update should be called once')
+
+  t.deepEqual(agent, single, 'agent should be the same')
 })
