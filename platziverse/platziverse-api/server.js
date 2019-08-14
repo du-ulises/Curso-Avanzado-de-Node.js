e30ed6135ed2f290947d2e149c79546c616e0303
Diego Ulises <diegoulisesmartinezaguilar@gmail.com>
2019-08-14 12:32:00 -0500

    Definición de rutas del API

--- platziverse/platziverse-api/server.js
+++ platziverse/platziverse-api/server.js
@@ -1,14 +1,17 @@
 'use strict'
 
-const http = require('http') //import http from 'http'
+const http = require('http') // import http from 'http'
 const chalk = require('chalk')
-const express = require('express') //import express from 'express'
+const express = require('express') // import express from 'express'
+
+const api = require('./api')
 
 const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
 
-server.listen(port, () => {
-    console.log(`${chalk.green('[platziverse-api')} server listening on port ${port}`)
-})
+app.use('/api', api)
 
+server.listen(port, () => {
+  console.log(`${chalk.green('[platziverse-api')} server listening on port ${port}`)
+})
